00b518181dd6b0374a36a15d8aca11bd8753b174
asrankulovanargiza07 <70037062+asrankulovanargiza07@users.noreply.github.com>
2021-05-11 13:34:06 +0600

    Web Chat

---
+++ ChatClubEnglishSpeakers/Publico/Hubs/ChatHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Publico.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Publico.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(Message message) =>
+            await Clients.All.SendAsync("receiveMessage", message);
+
+        //public async Task GetGmail(string gmail)
+        //{
+        //    Client client = new Client
+        //    {
+
+        //    }
+        //}
+    }
+}
